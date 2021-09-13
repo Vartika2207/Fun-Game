76899a44efa1092a91b47a4474dd42bd6bf654d6
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2021-09-14 00:05:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+//your age in days
+function ageInDays(){
+    var birthYear = prompt('In which year you were born in ? ')
+    var ageInDayss = (2021 - birthYear) * 365;
+    var h1 = document.createElement('h1')
+    var textAnswer = document.createTextNode('You are ' + ageInDayss + ' days old.');
+    h1.setAttribute('id', 'ageInDays');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1);
+}
+
+function reset(){
+    document.getElementById('ageInDays').remove();
+}
